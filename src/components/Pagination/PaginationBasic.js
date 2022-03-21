3ef62d79dc6fa6d38b62a1fcd89d406c73506955
Yemelyanov <nikolaef.post@gmail.com>
2022-03-21 15:52:48 +0200

    added NewsPage.js  Pagination mobil test

--- src/components/Pagination/PaginationBasic.js
+++ src/components/Pagination/PaginationBasic.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import {Pagination} from "react-bootstrap";
+import styles from "./Pagination.module.scss"
 
 const PaginationBasic = ({active, numb, paginHandle}) => {
     let items = [];
     for (let number = 1; number <= numb; number++) {
-        console.log(number)
         items.push(
             <Pagination.Item key={number} active={number === active}
-                             onClick={()=>paginHandle(number)}
+                             onClick={()=>paginHandle(number)} className={styles.color}
             >
                 {number}
             </Pagination.Item>,
         );
     }
-    console.log(items)
     return (
         <div>
-            <Pagination>{items}</Pagination>
+            <Pagination >{items}</Pagination>
         </div>
     );
 };
--- src/page/NewsPage.jsx
+++ src/page/NewsPage.jsx
@@ -6,10 +6,12 @@ import styles from "./page.module.scss"
 import {Container, Row} from "react-bootstrap";
 import NewsTitleW from "../assets/images/NewsTitleW";
 
-const NewsPage = ({screenWidth}) => {
+const NewsPage = ({screenWidth, scrollPosition}) => {
     const [news, setNews] = useState( [])
     const [page, setPage] = useState(0)
+    let scrollHeight = document.body.scrollHeight - document.body.scrollTop
 
+    console.log(news, page, scrollHeight)
 
     const { loading, error, data, fetchMore} = useQuery(GET_NEWS1, {
         variables: {
@@ -17,27 +19,46 @@ const NewsPage = ({screenWidth}) => {
         },
     });
 
-    useEffect(()=>{
-        if (data) setNews(data.queryNews)
-    },[data])
+    // useEffect(()=>{
+    //     if (data) setNews(data.queryNews)
+    // },[data])
 
     useEffect(()=>{
+        console.log('OPPPPPPP')
         fetchMore({
             variables: {
                 offset: page*4,
             },
         }).then(fetchMoreResult => {
-            if (fetchMoreResult) setNews(fetchMoreResult.data.queryNews)
+            if (fetchMoreResult && (screenWidth > 600)) setNews(fetchMoreResult.data.queryNews)
+            else if (screenWidth < 600) setNews((pre)=>[...pre, ...fetchMoreResult.data.queryNews])
 
         });
     },[page])
 
+    useEffect(()=>{
+        console.log( scrollPosition , 1500)
+        if(scrollPosition > scrollHeight) setPage((pre)=>pre+1)
+    },[scrollPosition])
+
     function paginHandle(n){setPage(n-1)}
+
+    // function onScrollList(event) {
+    //     const scrollBottom = event.target.scrollTop +
+    //         event.target.offsetHeight === event.target.scrollHeight;
+    //     console.log(scrollBottom)
+    //
+    //     if (scrollBottom) {
+    //         console.log('OPPPPPPP')
+    //         // setPage((pre)=>pre+1)
+    //     }
+    // }
+
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>{error}</p>;
     return (
-        <div className={styles.wraper}>
+        <div className={styles.wraper} >
             <div>
                 <NavBar/>
                 <div className={styles.title}>

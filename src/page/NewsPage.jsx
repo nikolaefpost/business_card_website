231eb9a089a60011e8300e8a1a8729bdd9839ca0
Yemelyanov <nikolaef.post@gmail.com>
2022-03-22 11:16:00 +0200

    added NewsPage.js  Pagination mobil load by scroll

--- src/page/NewsPage.jsx
+++ src/page/NewsPage.jsx
@@ -5,15 +5,16 @@ import {CardNews, NavBar, PaginationBasic} from "../components";
 import styles from "./page.module.scss"
 import {Container, Row} from "react-bootstrap";
 import NewsTitleW from "../assets/images/NewsTitleW";
+import {logDOM} from "@testing-library/react";
 
 const NewsPage = ({screenWidth, scrollPosition}) => {
-    const [news, setNews] = useState( [])
+    const [news, setNews] = useState([])
     const [page, setPage] = useState(0)
-    let scrollHeight = document.body.scrollHeight - document.body.scrollTop
+    let scrollHeight = document.body.scrollHeight - document.body.clientHeight
+    console.log('scrollHeight', scrollHeight)
+    console.log('news, page', news, page)
 
-    console.log(news, page, scrollHeight)
-
-    const { loading, error, data, fetchMore} = useQuery(GET_NEWS1, {
+    const {loading, error, data, fetchMore} = useQuery(GET_NEWS1, {
         variables: {
             first: 4,
         },
@@ -23,61 +24,58 @@ const NewsPage = ({screenWidth, scrollPosition}) => {
     //     if (data) setNews(data.queryNews)
     // },[data])
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('OPPPPPPP')
         fetchMore({
-            variables: {
-                offset: page*4,
-            },
+            variables: (screenWidth > 600) ?
+                {
+                    offset: page * 4,
+                    first: 4,
+                } :
+                {
+                    offset: page,
+                    first: 1,
+                }
         }).then(fetchMoreResult => {
             if (fetchMoreResult && (screenWidth > 600)) setNews(fetchMoreResult.data.queryNews)
-            else if (screenWidth < 600) setNews((pre)=>[...pre, ...fetchMoreResult.data.queryNews])
+            else if (screenWidth < 600) setNews((pre) => [...pre, ...fetchMoreResult.data.queryNews])
 
         });
-    },[page])
+    }, [page])
 
-    useEffect(()=>{
-        console.log( scrollPosition , 1500)
-        if(scrollPosition > scrollHeight) setPage((pre)=>pre+1)
-    },[scrollPosition])
+    useEffect(() => {
+        console.log('scrollPosition', scrollPosition)
+        if (scrollPosition > 100 && scrollPosition === scrollHeight) setPage((pre) => pre + 1)
+    }, [scrollPosition])
 
-    function paginHandle(n){setPage(n-1)}
-
-    // function onScrollList(event) {
-    //     const scrollBottom = event.target.scrollTop +
-    //         event.target.offsetHeight === event.target.scrollHeight;
-    //     console.log(scrollBottom)
-    //
-    //     if (scrollBottom) {
-    //         console.log('OPPPPPPP')
-    //         // setPage((pre)=>pre+1)
-    //     }
-    // }
-
+    function paginHandle(n) {
+        setPage(n - 1)
+    }
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>{error}</p>;
     return (
-        <div className={styles.wraper} >
+        <div className={styles.wraper}>
             <div>
                 <NavBar/>
                 <div className={styles.title}>
-                    <NewsTitleW width={(screenWidth > 600)? '350' : '300'}/>
+                    <NewsTitleW width={(screenWidth > 600) ? '350' : '300'}/>
                     <p className={styles.title_text}>
                         В этом разделе вы найдете ответы на часто задаваемые вопросы. Наверняка они окажутся полезными.
-                        Если нужная тема в этом перечне не будет освещена – вы всегда можете задать вопрос лично Виктории.
+                        Если нужная тема в этом перечне не будет освещена – вы всегда можете задать вопрос лично
+                        Виктории.
                     </p>
                 </div>
                 <Container className=''>
                     <Row>
-                        {news.map(item=>
-                            <CardNews key={item.id} desc={true}  news={item} col={6}/>
+                        {news.map(item =>
+                            <CardNews key={item.id} desc={true} news={item} col={6}/>
                         )}
                     </Row>
                 </Container>
             </div>
 
-            <PaginationBasic numb={2} active={page+1} paginHandle={paginHandle}/>
+            {(screenWidth > 600) && <PaginationBasic numb={2} active={page + 1} paginHandle={paginHandle}/>}
         </div>
     );
 };
